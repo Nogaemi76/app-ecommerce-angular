06ab14d5dabfddb5cfce0cf76034b7f96de38ce8
Emilie <milogare@yahoo.fr>
2023-02-08 17:26:35 +0100

    Added Firebase, AngularFire, AngularFirestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app-ecommerce-angular';
+export class AppComponent implements OnInit {
+  courses!: Observable<any[]>;
+
+  constructor(private db: AngularFirestore) {}
+
+  ngOnInit(): void {
+    this.courses = this.db.collection('courses').valueChanges();
+  }
 }
