f7027fd37718ffeb59e568792ecc918b495d2337
Emilie <milogare@yahoo.fr>
2023-02-13 18:14:13 +0100

    Added tabs to coursesComponent

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 const modules: any[] = [
   MatToolbarModule,
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatTabsModule
 ];
 
 @NgModule({
--- src/app/modules/common/common.module.ts
+++ src/app/modules/common/common.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from 'src/app/material.module';
+
+import { AboutComponent } from './components/about/about.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [],
-  imports: []
+  declarations: [
+    HomeComponent,
+    CoursesComponent,
+    AboutComponent,
+    LoginComponent
+  ],
+  imports: [
+    MaterialModule,
+  ]
 })
 export class CommonModule { }
