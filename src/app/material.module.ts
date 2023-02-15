73155c8c202e92e1696aaeae118b0eeea30a9bcb
Emilie <milogare@yahoo.fr>
2023-02-15 04:48:54 +0100

    Added & styled courses with mat-card in coursesComponent

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,12 +3,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 const modules: any[] = [
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
-  MatTabsModule
+  MatTabsModule,
+  MatCardModule
 ];
 
 @NgModule({
