f7ac712ac08d2be8fcdb081d1b261e99efd9c638
Emilie <milogare@yahoo.fr>
2023-03-02 00:57:28 +0100

    Added AngularFireAuthGuard to admin-courses with redirection to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
 import { AboutComponent } from './modules/pages/components/about/about.component';
 import { CoursesComponent } from './modules/pages/components/courses/courses.component';
 import { HomeComponent } from './modules/pages/components/home/home.component';
 import { AdminCoursesComponent } from './modules/admin/components/admin-courses/admin-courses.component';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['home']);
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'courses', component: CoursesComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'admin-courses', component: AdminCoursesComponent },
+  { path: 'admin-courses', component: AdminCoursesComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToHome} },
   { path: '**', component: HomeComponent }
 ];
 
